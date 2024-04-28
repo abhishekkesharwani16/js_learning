3fe8bb99f6b18d704e028a25bdd56c1a819b9b0d
abhishekkesharwani16 <86963912+abhishekkesharwani16@users.noreply.github.com>
2024-04-28 10:27:26 +0000

    strings pr charcha

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(2<1)
+// console.log(2<=1)
+// console.log(2>1)
+// console.log(2!=1)
+
+
+// console.log("2">1)
+// console.log("02">1)
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+console.log(undefined == 0);
+console.log(undefined > 0 );
+console.log(undefined< 0);
+
+// these types of comparisons avoid only same datatype to be follow , It's good idea
+
+
+// ===
+
+console.log("2"===2)
