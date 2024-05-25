8ffeb4a4120f805d35970f04cd4eb3e283f58f7d
abhishekkesharwani16 <86963912+abhishekkesharwani16@users.noreply.github.com>
2024-05-25 19:11:52 +0000

    scope level and hoisting

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,55 @@
+// var c = 300
+let a = 300
+
+if (true) {
+    let a ;
+    const b =20
+    // console.log("Inner: " , a);
+}
+// console.log(b);
+// console.log(c);
+// console.log(a);
+
+
+function one()  {
+    const username = "hitesh"
+    
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+
+}
+// one()
+
+
+if (true){
+    const username = "hitesh"
+    if(username === "hitesh"){
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+
+// ************************* Intresting **************************
+
+console.log(addone(5));
+
+function addone(num){
+    return num + 1
+}
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+    }
+    
